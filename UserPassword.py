e49f46dcf0f83624018046a14958a330cd6d2054
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-09-23 12:40:14 +0200

    Editing UserPassword class. Changing verify_login function and verify_password function

--- UserPassword.py
+++ UserPassword.py
@@ -18,11 +18,15 @@ class UserPassword():
         pwdhash = binascii.hexlify(pwdhash)
         return (salt + pwdhash).decode('ascii')
 
-    def verify_password(self, stored_password, provided_password):
+    def verify_password(self, db):
+        sql_command = 'select password from users where name=?;'
+        cursor = db.execute(sql_command, [self.name])
+        password_record = cursor.fetchone()
+        stored_password = password_record['password']
         """Verify a stored password against one provided by user"""
         salt = stored_password[:64]
         stored_password = stored_password[64:]
-        pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password.encode('utf-8'),
+        pwdhash = hashlib.pbkdf2_hmac('sha512', self.password.encode('utf-8'),
         salt.encode('ascii'), 100000)
         pwdhash = binascii.hexlify(pwdhash).decode('ascii')
         return pwdhash == stored_password 
@@ -39,7 +43,7 @@ class UserPassword():
         cursor = db.execute(sql_command, [self.name])
         user_record = cursor.fetchone()
 
-        if user_record != None and self.verify_password(user_record['password'], self.password):
+        if user_record != None and self.verify_password(db):
             return user_record
         else:
             self.user = None
